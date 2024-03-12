26e29e9e92fa549bcdacc38578b4d1419ad02dc0
Mayank Diwate <MayankDiwate007@gmail.com>
2024-03-12 21:50:54 +0530

    created routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
-function App() {
-  return <div>Hello World</div>;
-}
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
 export default App;
