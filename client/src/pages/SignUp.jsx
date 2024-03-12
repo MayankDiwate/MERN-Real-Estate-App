f7ab4806befe932ed2beb3ea85438af6280c419d
Mayank Diwate <MayankDiwate007@gmail.com>
2024-03-12 23:57:06 +0530

    created SignUp UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,62 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">SignUp</h1>
+      <form className="flex flex-col gap-4" >
+        <input
+          type="text"
+          placeholder="Username"
+          className="border p-3 rounded-lg"
+          id="username"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          className="
+            bg-slate-700
+            text-white
+            p-3
+            rounded-lg
+            uppercase
+            hover:opacity-95
+            disabled:opacity-80"
+        >
+          SignUp
+        </button>
+        <div className="flex gap-1 justify-end">
+          <p>Already have an account?</p>
+          <Link to="/signIn">
+            <span className="text-blue-700">Sign In</span>
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
