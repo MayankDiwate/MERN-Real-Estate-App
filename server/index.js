5f98215fc669e4108f7b478d09e167b5e9b012ec
Mayank Diwate <mayankdiwate007@gmail.com>
2024-11-06 23:38:32 +0530

    minor change

--- server/index.js
+++ server/index.js
@@ -32,6 +32,10 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 app.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
