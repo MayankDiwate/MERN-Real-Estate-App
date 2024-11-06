c99b8ae35f61ccecd529166d451adf1f25eceac6
Mayank Diwate <mayankdiwate007@gmail.com>
2024-11-07 00:09:24 +0530

    changed router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import About from "./pages/About";
@@ -14,7 +14,7 @@ import UpdateListing from "./pages/UpdateListing";
 const App = () => {
   return (
     <>
-      <Router>
+      <Router basename="/">
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -26,7 +26,10 @@ const App = () => {
           <Route element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
             <Route path="/create-listing" element={<CreateListing />} />
-            <Route path="/update-listing/:listingId" element={<UpdateListing />} />
+            <Route
+              path="/update-listing/:listingId"
+              element={<UpdateListing />}
+            />
           </Route>
         </Routes>
       </Router>
